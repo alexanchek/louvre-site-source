cf65e2ed5cb4fadb343b8b632fb98ca2ac23647e
alexanchek <58663068+alexanchek@users.noreply.github.com>
2021-08-21 22:55:36 +0300

    refactor: navlinks code was shorter

--- src/components/Header/Menu/NavLinks.js
+++ src/components/Header/Menu/NavLinks.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { MenuLink } from './MenuLink'
+import MenuLinks from './MenuLinks.json'
 
 export const NavLinks = ({isMobile, closeMobileMenu, openClass}) => {
     return (
         <nav className={`header__nav ${openClass}`}>
             <ul className="header__list">
-                <li className="header__item"><MenuLink classStyle={'header__link'} link={"visiting"} label={"Visiting"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li>
-                <li className="header__item" ><MenuLink classStyle={'header__link'} link={"picture-explore"} label={"Explore"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li>
-                <li className="header__item" ><MenuLink classStyle={'header__link'} link={"video-journey"} label={"Video"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li>
-                <li className="header__item" ><MenuLink classStyle={'header__link'} link={"gallery"} label={"Gallery"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li>
-                <li className="header__item"><MenuLink classStyle={'header__link'} link={"buy-tickets"} label={"Tickets"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li>
-                <li className="header__item"><MenuLink classStyle={'header__link'} link={"contact"} label={"Contact"} isMobile={isMobile} closeMobileMenu={closeMobileMenu}/></li> 
+                {
+                    MenuLinks.map(({link, label}) => {
+                        return (
+                            <li className="header__item" >
+                                <MenuLink classStyle={'header__link'} 
+                                link={link} 
+                                label={label} 
+                                isMobile={isMobile} 
+                                closeMobileMenu={closeMobileMenu}/></li>
+                        )
+                    })
+                }
             </ul>
         </nav>
     )
